20ac2aae484ef8662f74fb2a44e20e0fc3578b9c
John Benfer <jbenfer@ksu.edu>
2019-08-26 13:44:33 -0500

    1st commit

---
+++ Windows Assistant/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Windows_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.TextOutput = new System.Windows.Forms.TextBox();
+            this.TextInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.Location = new System.Drawing.Point(12, 48);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(151, 67);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Record";
+            this.RecordButton.UseVisualStyleBackColor = true;
+            this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(805, 48);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(151, 67);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // TextOutput
+            // 
+            this.TextOutput.Location = new System.Drawing.Point(12, 130);
+            this.TextOutput.Multiline = true;
+            this.TextOutput.Name = "TextOutput";
+            this.TextOutput.ReadOnly = true;
+            this.TextOutput.Size = new System.Drawing.Size(944, 670);
+            this.TextOutput.TabIndex = 2;
+            // 
+            // TextInput
+            // 
+            this.TextInput.Location = new System.Drawing.Point(189, 48);
+            this.TextInput.Multiline = true;
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(588, 66);
+            this.TextInput.TabIndex = 3;
+            this.TextInput.TextChanged += new System.EventHandler(this.TextInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 812);
+            this.Controls.Add(this.TextInput);
+            this.Controls.Add(this.TextOutput);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.RecordButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox TextOutput;
+        private System.Windows.Forms.TextBox TextInput;
+    }
+}
+
---
+++ Windows Assistant/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Windows_Assistant
+{
+    public partial class Form1 : Form
+    {
+
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Choices choices = new Choices();
+        
+        //choices.Add(new string[] {"hello", "open"});
+
+        public Form1()
+        {
+            InitializeComponent();
+            choices.Add(new string[] { "hello", "open chrome", "shutdown"});
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RecordButton_Click(object sender, EventArgs e)
+        {
+            RecordButton.Enabled = false;
+            StopButton.Enabled = true;
+            Grammar gr = new Grammar(new GrammarBuilder(choices));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechDetected += sre_SpeechDetected;
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void sre_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            string s = e.ToString() + Environment.NewLine;
+            TextOutput.Text += s;
+        }
+
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+
+                    break;
+                case "open chrome":
+                    Process.Start("chrome", "http://www.google.com");
+                    break;
+                case "shutdown":
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+
+            TextOutput.Text += e.Result.Text.ToString() + Environment.NewLine;
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            RecordButton.Enabled = true;
+            StopButton.Enabled = false;
+            sre.RecognizeAsyncStop();
+
+        }
+
+        private void TextInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
