1ff5c62a05c125e3cabe60065cbf5099c83404f9
John Benfer <jbenfer@ksu.edu>
2019-08-26 13:50:40 -0500

    my webhooks key

---
+++ Windows Assistant/IFTTT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Assistant
+{
+    class IFTTT
+    {
+        // key for webhooks api call.
+        string key = "cQ3xgk9nxLdnmZzs3VkXzv";
+
+    }
+}
