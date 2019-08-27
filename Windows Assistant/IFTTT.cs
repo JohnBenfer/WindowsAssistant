7af7e188b49cce902b8197c0ee459404c1da30d1
John Benfer <jbenfer@ksu.edu>
2019-08-26 23:30:14 -0500

    save

--- Windows Assistant/IFTTT.cs
+++ Windows Assistant/IFTTT.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using RestSharp;
+using System.Windows.Forms;
 
 namespace Windows_Assistant
 {
@@ -10,6 +14,41 @@ namespace Windows_Assistant
     {
         // key for webhooks api call.
         string key = "cQ3xgk9nxLdnmZzs3VkXzv";
+        string baseRestUrl;
+        string action = "bedroom_lights_on";
+        RestClient client;
+        RestRequest request;
+        
+        public bool executeAction(string a)
+        {
+            setAction(a, baseRestUrl);
 
+            return true;
+        }
+
+        private string setAction(string a, string url)
+        {
+            return url.Replace("event", a);
+        }
+
+        private string setKey(string k, string url)
+        {
+            return url.Replace("webhookskey", k);
+        }
+
+        public IFTTT (string key, string baseRest)
+        {
+            baseRestUrl = setKey(key, baseRest);
+            client = new RestClient(baseRestUrl);
+            request = new RestRequest("events/{events}", Method.POST);
+            MessageBox.Show(request.ToString());
+            request.AddUrlSegment("events", "bedroom_lights_off");
+            MessageBox.Show(request.ToString());
+
+
+            //client.BaseAddress = new Uri(post);
+            //HttpResponseMessage response = client.GetAsync()
+        }
+                     
     }
 }
