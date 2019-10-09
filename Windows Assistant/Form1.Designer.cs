5b068b68989ec3706e223a500f4ca25e0fe262e0
Jbenfer <Jbenfer@DESKTOP-8KA2QAH>
2019-10-09 15:18:36 -0500

    added google speech to text stuff

--- Windows Assistant/Form1.Designer.cs
+++ Windows Assistant/Form1.Designer.cs
@@ -32,13 +32,15 @@
             this.StopButton = new System.Windows.Forms.Button();
             this.TextOutput = new System.Windows.Forms.TextBox();
             this.TextInput = new System.Windows.Forms.TextBox();
+            this.google_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // RecordButton
             // 
-            this.RecordButton.Location = new System.Drawing.Point(12, 48);
+            this.RecordButton.Location = new System.Drawing.Point(11, 38);
+            this.RecordButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.RecordButton.Name = "RecordButton";
-            this.RecordButton.Size = new System.Drawing.Size(151, 67);
+            this.RecordButton.Size = new System.Drawing.Size(134, 54);
             this.RecordButton.TabIndex = 0;
             this.RecordButton.Text = "Record";
             this.RecordButton.UseVisualStyleBackColor = true;
@@ -46,9 +48,10 @@
             // 
             // StopButton
             // 
-            this.StopButton.Location = new System.Drawing.Point(805, 48);
+            this.StopButton.Location = new System.Drawing.Point(716, 38);
+            this.StopButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(151, 67);
+            this.StopButton.Size = new System.Drawing.Size(134, 54);
             this.StopButton.TabIndex = 1;
             this.StopButton.Text = "Stop";
             this.StopButton.UseVisualStyleBackColor = true;
@@ -56,31 +59,46 @@
             // 
             // TextOutput
             // 
-            this.TextOutput.Location = new System.Drawing.Point(12, 130);
+            this.TextOutput.Location = new System.Drawing.Point(11, 104);
+            this.TextOutput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextOutput.Multiline = true;
             this.TextOutput.Name = "TextOutput";
             this.TextOutput.ReadOnly = true;
-            this.TextOutput.Size = new System.Drawing.Size(944, 670);
+            this.TextOutput.Size = new System.Drawing.Size(840, 537);
             this.TextOutput.TabIndex = 2;
             // 
             // TextInput
             // 
-            this.TextInput.Location = new System.Drawing.Point(189, 48);
+            this.TextInput.Location = new System.Drawing.Point(168, 38);
+            this.TextInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextInput.Multiline = true;
             this.TextInput.Name = "TextInput";
-            this.TextInput.Size = new System.Drawing.Size(588, 66);
+            this.TextInput.Size = new System.Drawing.Size(523, 54);
             this.TextInput.TabIndex = 3;
             this.TextInput.TextChanged += new System.EventHandler(this.TextInput_TextChanged);
             // 
+            // google_button
+            // 
+            this.google_button.Location = new System.Drawing.Point(557, 38);
+            this.google_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.google_button.Name = "google_button";
+            this.google_button.Size = new System.Drawing.Size(134, 54);
+            this.google_button.TabIndex = 4;
+            this.google_button.Text = "Google API";
+            this.google_button.UseVisualStyleBackColor = true;
+            this.google_button.Click += new System.EventHandler(this.google_button_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(968, 812);
+            this.ClientSize = new System.Drawing.Size(860, 650);
+            this.Controls.Add(this.google_button);
             this.Controls.Add(this.TextInput);
             this.Controls.Add(this.TextOutput);
             this.Controls.Add(this.StopButton);
             this.Controls.Add(this.RecordButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -95,6 +113,7 @@
         private System.Windows.Forms.Button StopButton;
         private System.Windows.Forms.TextBox TextOutput;
         private System.Windows.Forms.TextBox TextInput;
+        private System.Windows.Forms.Button google_button;
     }
 }
 
--- Windows Assistant/Form1.cs
+++ Windows Assistant/Form1.cs
@@ -13,12 +13,18 @@ using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Threading;
 using System.Diagnostics;
+using Google.Cloud.Speech.V1;
+using Google.Apis.Auth.OAuth2;
+using Grpc.Auth;
+
 
 namespace Windows_Assistant
 {
     public partial class Form1 : Form
     {
 
+        SpeechClient speech = SpeechClient.Create();
+        
         SpeechSynthesizer ss = new SpeechSynthesizer();
         PromptBuilder pb = new PromptBuilder();
         SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
@@ -29,6 +35,7 @@ namespace Windows_Assistant
         {
             InitializeComponent();
             choices.Add(new string[] { "hello", "open chrome", "shutdown", "lights on", "lights off"});
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -38,6 +45,9 @@ namespace Windows_Assistant
 
         private void RecordButton_Click(object sender, EventArgs e)
         {
+            var client = SpeechClient.Create();
+            
+            
             RecordButton.Enabled = false;
             StopButton.Enabled = true;
             Grammar gr = new Grammar(new GrammarBuilder(choices));
@@ -102,5 +112,89 @@ namespace Windows_Assistant
         {
 
         }
+
+
+        // [START speech_transcribe_streaming_mic]
+        static async Task<object> StreamingMicRecognizeAsync(int seconds)
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "en",
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Read from the microphone and stream to API.
+            object writeLock = new object();
+            bool writeMore = true;
+            var waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            waveIn.DataAvailable +=
+                (object sender, NAudio.Wave.WaveInEventArgs args) =>
+                {
+                    lock (writeLock)
+                    {
+                        if (!writeMore)
+                        {
+                            return;
+                        }
+
+                        streamingCall.WriteAsync(
+                            new StreamingRecognizeRequest()
+                            {
+                                AudioContent = Google.Protobuf.ByteString
+                                    .CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                            }).Wait();
+                    }
+                };
+            waveIn.StartRecording();
+            Console.WriteLine("Speak now.");
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            // Stop recording and shut down.
+            waveIn.StopRecording();
+            lock (writeLock)
+            {
+                writeMore = false;
+            }
+
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+
+        private void google_button_Click(object sender, EventArgs e)
+        {
+            await StreamingMicRecognizeAsync(10);
+        }
+        // [END speech_transcribe_streaming_mic]
+
     }
 }
